eefe3cff838f0f7e1ea65b27d4295d78458b9b9a
MagnusMadsen <40494689+MagnusMadsen@users.noreply.github.com>
2026-04-21 09:58:13 +0200

    update af main.py til kun bootstrap

--- services/backend/app/main.py
+++ services/backend/app/main.py
@@ -1,24 +1,10 @@
-from flask import Flask, jsonify
-from db import get_connection
-from psycopg2.extras import RealDictCursor
-
-from capture import start_capture
-from parser import parse_packet
-
+from flask import Flask
 from api import api_bp
-from storage import save_packet
 from capture import start_capture_thread
-
-import threading
-import os
-
-CAPTURE_INTERFACE = os.getenv("CAPTURE_INTERFACE", "eth0")
 
 app = Flask(__name__)
 app.register_blueprint(api_bp)
 
-
 if __name__ == "__main__":
     start_capture_thread()
     app.run(host="0.0.0.0", port=8000, debug=False)
-    
