2a611349c8f3bc28d61a55bedfeb574c2bc7e2ab
MagnusMadsen <40494689+MagnusMadsen@users.noreply.github.com>
2026-04-21 12:41:12 +0200

    update af SQL kald

--- services/backend/app/db.py
+++ services/backend/app/db.py
@@ -9,3 +9,27 @@ def get_connection():
         user=os.getenv("DB_USER", "admin"),
         password=os.getenv("DB_PASSWORD", "Admin1234!"),
     )
+
+def init_db():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS observed_connections (
+            id SERIAL PRIMARY KEY,
+            src_ip INET NOT NULL,
+            dst_ip INET NOT NULL,
+            protocol TEXT NOT NULL,
+            src_port INTEGER,
+            dst_port INTEGER,
+            first_seen TIMESTAMP NOT NULL DEFAULT NOW(),
+            last_seen TIMESTAMP NOT NULL DEFAULT NOW(),
+            packet_count INTEGER NOT NULL DEFAULT 1,
+            UNIQUE (src_ip, dst_ip, protocol, src_port, dst_port)
+        );
+    """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+    
--- services/backend/app/main.py
+++ services/backend/app/main.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from api import api_bp
 from capture import start_capture_thread
+from db import init_db
 
 app = Flask(__name__)
 app.register_blueprint(api_bp)
 
 if __name__ == "__main__":
+    init_db()
     start_capture_thread()
     app.run(host="0.0.0.0", port=8000, debug=False)
+
+    
